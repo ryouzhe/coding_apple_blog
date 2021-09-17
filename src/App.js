c957afd742b65838aced11336a3b63b3367dd2ce
ryouzhe <sjh3183@naver.com>
2021-09-17 21:00:14 +0900

    useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable */
+
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [content, setContent] = useState(["React", "Redux", "Hooks"]);
+  const [counter, setCounter] = useState(0);
+
+  function changeContent() {
+    let newArray = [...content];
+    newArray[0] = "JavaScript";
+    setContent(newArray);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>개발 blog</div>
+      </div>
+      <button onClick={changeContent}>버튼</button>
+      <div className="list">
+        <h3>
+          {content[0]}{" "}
+          <span
+            onClick={() => {
+              setCounter(counter + 1);
+            }}
+          >
+            👍
+          </span>
+          {counter}
+        </h3>
+        <p>9월 17일 발행</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h3>{content[1]}</h3>
+        <p>9월 17일 발행</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h3>{content[2]}</h3>
+        <p>9월 17일 발행</p>
+        <hr />
+      </div>
     </div>
   );
 }
